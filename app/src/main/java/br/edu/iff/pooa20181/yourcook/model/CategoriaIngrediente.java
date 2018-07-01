0754d61f0fca8b504138128cfaedb15b17a87a63
bianca longhi <longhisbianca@gmail.com>
2018-07-01 17:26:41 -0300

    Criando model CategoriaIngrediente e configurando para utilizar o realm

---
+++ app/src/main/java/br/edu/iff/pooa20181/yourcook/model/CategoriaIngrediente.java
@@ -0,0 +1,49 @@
+package br.edu.iff.pooa20181.yourcook.model;
+
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class CategoriaIngrediente extends RealmObject implements Serializable {
+
+    @PrimaryKey
+    private int id;
+    private String nomeCategoria;
+    private String descricao;
+
+    public CategoriaIngrediente() {
+    }
+
+    public CategoriaIngrediente(int id, String nomeCategoria, String descricao) {
+        this.id = id;
+        this.nomeCategoria = nomeCategoria;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+}
