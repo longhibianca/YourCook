7aa729fe273155d8e11281239bad7ff73c075ddd
bianca longhi <longhisbianca@gmail.com>
2018-07-01 12:39:27 -0300

    Criando tela principal e tela de login

---
+++ app/src/main/java/br/edu/iff/pooa20181/yourcook/CadastrarUsuarioActivity.java
@@ -0,0 +1,13 @@
+package br.edu.iff.pooa20181.yourcook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_usuario);
+    }
+}
--- app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
@@ -1,13 +1,35 @@
 package br.edu.iff.pooa20181.yourcook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView entrar;
+    TextView registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        entrar = (TextView) findViewById(R.id.tvEntrar);
+        registrar = (TextView) findViewById(R.id.tvRegistrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CadastrarUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
