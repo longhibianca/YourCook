62c9ed02c6e46bb8e8c204724ee710ef93a6ac77
bianca longhi <longhisbianca@gmail.com>
2018-08-26 12:43:41 -0300

    definindo fluxo de sign in

--- app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
@@ -1,24 +1,32 @@
 package br.edu.iff.pooa20181.yourcook;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.signin.SignIn;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     //TextView entrar;
     TextView registrar;
     SignInButton signInButton;
     private GoogleSignInClient googleSignInClient;
+    private static final int RC_ENTRAR = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,5 +74,52 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,FiltroActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void signIn() {
+        //intent para mostrar com qual conta o usuário deseja entrar
+        Intent entrarIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(entrarIntent, RC_ENTRAR);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // sucesso ao logar
+            Intent intent = new Intent(MainActivity.this,FiltroActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            Toast.makeText(getApplicationContext(), "Algo de errado aconteceu", Toast.LENGTH_LONG);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_ENTRAR) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
