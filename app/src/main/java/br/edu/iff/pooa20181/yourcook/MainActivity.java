fbe95ca55568f089b779380e7e1c7bae7a87dfab
bianca longhi <longhisbianca@gmail.com>
2018-08-26 11:47:48 -0300

    fazendo configurações do google sign in button

--- app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/yourcook/MainActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.signin.SignIn;
+
 public class MainActivity extends AppCompatActivity {
 
     //TextView entrar;
     TextView registrar;
+    SignInButton signInButton;
+    private GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //entrar = (TextView) findViewById(R.id.tvEntrar);
         registrar = (TextView) findViewById(R.id.tvRegistrar);
-
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
        /* entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
+        signInButton.setOnClickListener((View.OnClickListener) this);
+
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,5 +41,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //adicionando config para pedir o email
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 }
